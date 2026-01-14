09b6cc78d7e360dde77429596c54f2d385946f6a
Pedro Linhares <plinfps@gmail.com>
2026-01-14 19:45:02 -0300

    add groupAnagrams resolution

--- LeetCode/GroupAnagrams49/groupAnagrams.java
+++ LeetCode/GroupAnagrams49/groupAnagrams.java
@@ -1,7 +1,18 @@
-import java.util.List;
+import java.util.*;
 
-class groupAnagram {
-    public List<List<String>> groupAnagrams(String[] strs) {
+public class groupAnagrams {
+    public List<List<String>> groupAnagrams (String[] strs) {
 
+        Map<String, List<String>> res = new HashMap<>();
+
+        for (String s : strs) {
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String sortedS = new String(charArray);
+
+            res.putIfAbsent(sortedS, new ArrayList<>());
+            res.get(sortedS).add(s);
+        }
+        return new ArrayList<>();
     }
 }
