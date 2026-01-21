86afdaa6ee45374099d3cf61d1939c35864e9e1f
Pedro Linhares <plinfps@gmail.com>
2026-01-21 17:38:48 -0300

    feat: add java class for resolution of encodeAndDecodeStrings

---
+++ LeetCode/EncodeAndDecodeStrings271/encodeAndDecodeStrings.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class encodeAndDecodeStrings {
+    public String encode (List<String> strs) {
+
+    }
+
+    public List<String> decode (String str) {
+
+    }
+}
