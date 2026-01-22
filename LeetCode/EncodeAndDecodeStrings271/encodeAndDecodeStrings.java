c30a0fb90185e9438443e47f0c53791c6b3d3af9
Pedro Linhares <plinfps@gmail.com>
2026-01-22 16:33:28 -0300

    feat: finished code encodeAndDecodeStrings.java

--- LeetCode/EncodeAndDecodeStrings271/encodeAndDecodeStrings.java
+++ LeetCode/EncodeAndDecodeStrings271/encodeAndDecodeStrings.java
@@ -1,11 +1,32 @@
+import java.util.ArrayList;
 import java.util.List;
 
-public class encodeAndDecodeStrings {
-    public String encode (List<String> strs) {
-
+class Solution {
+    public String encode(List<String> strs) {
+        StringBuilder encodedString = new StringBuilder();
+        for (String string : strs) {
+            encodedString.append(string.length());
+            encodedString.append("#");
+            encodedString.append(strs);
+        }
+        return encodedString.toString();
     }
 
-    public List<String> decode (String str) {
+    public List<String> decode(String str) {
+        List<String> result = new ArrayList<>();
+        int i = 0;
 
+        while (i < str.length()) {
+            int j = i;
+
+            while (str.charAt(j) != '#') {
+
+                int length = Integer.parseInt(str.substring(i, j));
+                String word = str.substring(j + 1, j + 1 + length);
+                result.add(word);
+                i = j + 1 + length;
+            }
+        }
+        return result;
     }
 }
