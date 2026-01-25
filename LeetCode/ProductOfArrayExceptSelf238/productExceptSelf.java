2a4f6173a8a202dff4ba6a4cdfcca693b2a9baa9
Pedro Linhares <plinfps@gmail.com>
2026-01-25 00:10:57 -0300

    feat: resolution of productExceptSelf

--- LeetCode/ProductOfArrayExceptSelf238/productExceptSelf.java
+++ LeetCode/ProductOfArrayExceptSelf238/productExceptSelf.java
@@ -1,5 +1,23 @@
 public class productExceptSelf {
-    public int[] exceptSelf(int[] nums) {
+    public int[] productExceptSelf(int[] nums) {
 
+        int n = nums.length;
+        int[] preff = new int[n];
+        int[] suff = new int[n];
+        int[] result = new int[n];
+
+        preff[0] = 1;
+        suff[n-1] = 1;
+
+        for (int i=1; i<nums.length; i++) {
+            preff[i] = nums[i-1] * preff[i-1];
+        }
+        for (int i=n-2; i>=0; i--) {
+            suff[i] = nums[i+1] * suff[i+1];
+        }
+        for (int i=0; i< nums.length; i++) {
+            result[i] = preff[i] * suff[i];
+        }
+        return result;
     }
 }
