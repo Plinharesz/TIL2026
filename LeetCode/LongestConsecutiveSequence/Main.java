ab63563e22487f27629a6b2e27c8fa78ed27b7fd
Pedro Linhares <plinfps@gmail.com>
2026-01-28 18:11:22 -0300

    test: file to test values

--- LeetCode/LongestConsecutiveSequence/Main.java
+++ LeetCode/LongestConsecutiveSequence/Main.java
@@ -4,12 +4,15 @@ public class Main {
     public static void main(String[] args) {
     longestConsecutiveSequence solucao = new longestConsecutiveSequence();
 
-    int[] nums = new int[5];
-    nums[0] = 2;
-    nums[1] = 12;
-    nums[2] = 0;
-    nums[3] = 0;
-    nums[4] = 0;
+    int[] nums = new int[8];
+    nums[0] = 0;
+    nums[1] = 3;
+    nums[2] = 2;
+    nums[3] = 5;
+    nums[4] = 4;
+    nums[5] = 6;
+    nums[6] = 1;
+    nums[7] = 1;
 
         System.out.println(solucao.longestConsecutiveSequence(nums));
 
