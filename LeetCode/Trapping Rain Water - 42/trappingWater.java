8ddd95148358037f5cb387cd6d6253928b938a83
Pedro Linhares <plinfps@gmail.com>
2026-02-12 18:51:12 -0300

    feat: resolution for trappingWater

--- LeetCode/Trapping Rain Water - 42/trappingWater.java
+++ LeetCode/Trapping Rain Water - 42/trappingWater.java
@@ -1,5 +1,30 @@
 public class trappingWater {
     public int trap(int[] heights) {
 
+        int leftPointer = 0;
+        int rightPointer = heights.length-1;
+        int maxLeft = 0;
+        int maxRight = 0;
+        int totalWater = 0;
+
+        while (leftPointer < rightPointer) {
+            if (heights[leftPointer] <= heights[rightPointer]) {
+                if (heights[leftPointer] >= maxLeft) {
+                    maxLeft += heights[leftPointer];
+                } else {
+                    totalWater += maxLeft - heights[leftPointer];
+                }
+                leftPointer++;
+            }
+            else {
+                if (heights[rightPointer] >= maxRight) {
+                    maxRight = heights[rightPointer];
+                } else {
+                    totalWater += maxRight - heights[rightPointer];
+                }
+                rightPointer--;
+            }
+        }
+        return totalWater;
     }
 }
