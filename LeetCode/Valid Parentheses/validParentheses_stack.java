d6fbe16d4afe4575e015c19232b3e1ce8afd6ef0
Pedro Linhares <plinfps@gmail.com>
2026-02-18 01:25:23 -0300

    feat: add resolution (not finished)

--- LeetCode/Valid Parentheses/validParentheses_stack.java
+++ LeetCode/Valid Parentheses/validParentheses_stack.java
@@ -1,5 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class validParentheses_stack {
     public boolean isValid (String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> closeToOpen = new HashMap<>();
 
+        closeToOpen.put(')', '(');
+        closeToOpen.put(']', '[');
+        closeToOpen.put('}', '{');
+
+        for (char c : s.toCharArray()) {
+            if (closeToOpen.containsKey(c)) {
+
+            }
+        }
+        return false;
     }
 }
