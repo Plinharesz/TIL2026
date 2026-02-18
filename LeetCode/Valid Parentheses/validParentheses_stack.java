54b48cee1f1c6edbf22634636dafc1e5a0951abc
Pedro Linhares <plinfps@gmail.com>
2026-02-18 15:18:23 -0300

    feat: stack resolution for validParentheses

--- LeetCode/Valid Parentheses/validParentheses_stack.java
+++ LeetCode/Valid Parentheses/validParentheses_stack.java
@@ -7,15 +7,23 @@ public class validParentheses_stack {
         Stack<Character> stack = new Stack<>();
         Map<Character, Character> closeToOpen = new HashMap<>();
 
-        closeToOpen.put(')', '(');
-        closeToOpen.put(']', '[');
-        closeToOpen.put('}', '{');
+        closeToOpen.put(')','(');
+        closeToOpen.put(']','[');
+        closeToOpen.put('}','{');
 
         for (char c : s.toCharArray()) {
             if (closeToOpen.containsKey(c)) {
-
+                if (!stack.isEmpty() && stack.peek() == closeToOpen.get(c)) {
+                    stack.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                stack.push(c);
             }
         }
-        return false;
+        return stack.isEmpty();
     }
 }
