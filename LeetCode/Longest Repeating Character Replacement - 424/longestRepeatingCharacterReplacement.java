9b7c70ca829b1be90c1f820541d89fd831390c54
plinha <plinfps@gmail.com>
2026-03-27 12:07:34 -0300

    feat: java class for longestRepeatingCharacterReplacement

--- LeetCode/Longest Repeating Character Replacement - 424/longestRepeatingCharacterReplacement.java
+++ LeetCode/Longest Repeating Character Replacement - 424/longestRepeatingCharacterReplacement.java
@@ -1,5 +1,20 @@
+import java.util.HashSet;
+
 public class longestRepeatingCharacterReplacement {
     public longestRepeatingCharacterReplacement (String s, int k) {
+        int res = 0;
+        HashSet<Character> charSet = new HashSet<>();
 
+        for (char c : s.toCharArray()) {
+            charSet.add(c);
+        }
+        for (char c : charSet);
+        int count =0;
+        int l = 0;
+        for (int r = 0; r < s.length(); r++) {
+            if(s.charAt(r) == c) {
+
+            }
+        }
     }
 }
